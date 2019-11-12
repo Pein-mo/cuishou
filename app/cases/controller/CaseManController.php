2b6639d918fa5a02858d6ab7d03c941930954359
mozhenzhu <251439200@qq.com>
2019-11-12 22:20:00 +0800

    v1.0

---
+++ app/cases/controller/CaseManController.php
@@ -0,0 +1,43 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2019 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\cases\controller;
+
+use cmf\controller\AdminBaseController;
+use think\Db;
+use app\admin\model\AdminMenuModel;
+
+class CaseManController extends AdminBaseController
+{
+
+    public function initialize()
+    {
+        $adminSettings = cmf_get_option('admin_settings');
+        if (empty($adminSettings['admin_password']) || $this->request->path() == $adminSettings['admin_password']) {
+            $adminId = cmf_get_current_admin_id();
+            if (empty($adminId)) {
+                session("__LOGIN_BY_CMF_ADMIN_PW__", 1);//设置后台登录加密码
+            }
+        }
+
+        parent::initialize();
+    }
+
+    /**
+     * 后台首页
+     */
+    public function index()
+    {
+
+
+        $this->assign('data',[]);
+        return $this->fetch();
+    }
+}
